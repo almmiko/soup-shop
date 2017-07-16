b63fe559fd5c726330757e2336ad3dcb472a55e8
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-07-16 21:41:54 +0300

    🚀  init project

---
+++ shared/components/DemoApp/AsyncHomeRoute/HomeRoute.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Helmet from 'react-helmet';
+
+// import config from '../../../../config';
+
+function HomeRoute() {
+  return <div>some</div>
+}
+
+export default HomeRoute;
