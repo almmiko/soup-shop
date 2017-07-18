31d7a488b49c82cd815410a36f216b0ce033e98f
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-07-18 12:15:18 +0300

    add jest ts support

--- shared/components/DemoApp/AsyncHomeRoute/__tests__/HomeRoute.test.tsx
+++ shared/components/DemoApp/AsyncHomeRoute/__tests__/HomeRoute.test.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 import React from 'react';
 import { shallow } from 'enzyme';
+import {} from 'jest';
 
 import HomeRoute from '../HomeRoute';
 
